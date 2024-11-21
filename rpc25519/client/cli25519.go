844d2902938236ab8a822a3fb38536bf7089dabb
Jason E. Aten <j.e.aten@gmail.com>
2024-11-20 19:08:40 -0600

    add rpc25519 benchmark

---
+++ rpc25519/client/cli25519.go
@@ -0,0 +1,318 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	stdlog "log"
+	"reflect"
+	//"net"
+	//"net/rpc"
+	"os"
+	"sync"
+	"sync/atomic"
+	"time"
+
+	"github.com/glycerine/rpc25519"
+	//codec "github.com/mars9/codec"
+	//"github.com/rpcxio/rpcx-benchmark/proto"
+	"github.com/rpcxio/rpcx-benchmark/stat"
+	"github.com/smallnest/rpcx/log"
+	"go.uber.org/ratelimit"
+)
+
+var (
+	concurrency = flag.Int("c", 1, "concurrency")
+	total       = flag.Int("n", 1, "total requests for all clients")
+	host        = flag.String("s", "127.0.0.1:8972", "server ip and port")
+	pool        = flag.Int("pool", 10, " shared grpc clients")
+	rate        = flag.Int("r", 0, "throughputs")
+)
+
+/*
+func mainOrig() {
+	flag.Parse()
+
+	log.SetFlags(log.LstdFlags | log.Lshortfile) // Add Lshortfile for short file names
+	//log.SetLogger(log.NewDefaultLogger(os.Stdout, "", stdlog.LstdFlags|stdlog.Lshortfile, log.LvInfo))
+
+	var rl ratelimit.Limiter
+	if *rate > 0 {
+		rl = ratelimit.New(*rate)
+	}
+	// number of concurrent goroutines. Simulate client.
+	n := *concurrency
+	// number of requests each client needs to send.
+	m := *total / n
+	log.Printf("concurrency: %d\nrequests per client: %d\n\n", n, m)
+
+	name := "Hello.Say"
+
+	args := proto.PrepareArgs()
+
+	// parameter size
+	b := make([]byte, 1024)
+	i, _ := args.MarshalTo(b)
+	log.Printf("message size: %d bytes\n\n", i)
+
+	// Wait for all tests to complete.
+	var wg sync.WaitGroup
+	wg.Add(n * m)
+
+	// Total number of requests.
+	var trans uint64
+	// Return the total number of responses that were normal.
+	var transOK uint64
+
+	// Time consumption for each goroutine.
+	d := make([][]int64, n, n)
+
+	// create a client connection pool
+	var clientIndex uint64
+	poolClients := make([]*rpc.Client, 0, *pool)
+	for i := 0; i < *pool; i++ {
+		conn, err := net.Dial("tcp", *host)
+		if err != nil {
+			log.Fatal("dialing:", err)
+		}
+		c := codec.NewClientCodec(conn)
+		client := rpc.NewClientWithCodec(c)
+
+		// warmup
+		var reply proto.BenchmarkMessage
+		for j := 0; j < 5; j++ {
+			client.Call(name, args, &reply)
+		}
+		poolClients = append(poolClients, client)
+	}
+
+	// Fence: control the client to start testing at the same time.
+	var startWg sync.WaitGroup
+	startWg.Add(n + 1) // +1 is because there is a goroutine to record the start time.
+
+	// create a client goroutine and test it.
+	startTime := time.Now().UnixNano()
+	go func() {
+		startWg.Done()
+		startWg.Wait()
+		startTime = time.Now().UnixNano()
+	}()
+
+	for i := 0; i < n; i++ {
+		dt := make([]int64, 0, m)
+		d = append(d, dt)
+
+		go func(i int) {
+			var reply proto.BenchmarkMessage
+			startWg.Done()
+			startWg.Wait()
+
+			for j := 0; j < m; j++ {
+				// Current limiting, here the current waiting time is not counted into the waiting time.
+				if rl != nil {
+					rl.Take()
+				}
+
+				t := time.Now().UnixNano()
+				ci := atomic.AddUint64(&clientIndex, 1)
+				ci = ci % uint64(*pool)
+				client := poolClients[int(ci)]
+				err := client.Call(name, args, &reply)
+				t = time.Now().UnixNano() - t
+
+				d[i] = append(d[i], t)
+
+				if err == nil && reply.Field1 == "OK" {
+					atomic.AddUint64(&transOK, 1)
+				}
+				// if err != nil {
+				// 	log.Error(err)
+				// }
+
+				atomic.AddUint64(&trans, 1)
+				wg.Done()
+			}
+		}(i)
+
+	}
+
+	wg.Wait()
+
+	// statistics
+	stat.Stats(startTime, *total, d, trans, transOK)
+}
+*/
+
+func main() {
+	flag.Parse()
+
+	//log.SetFlags(log.LstdFlags | log.Lshortfile) // Add Lshortfile for short file names
+	log.SetLogger(log.NewDefaultLogger(os.Stdout, "", stdlog.LstdFlags|stdlog.Lshortfile, log.LvInfo))
+
+	var rl ratelimit.Limiter
+	if *rate > 0 {
+		rl = ratelimit.New(*rate)
+	}
+	// number of concurrent goroutines. Simulate client.
+	n := *concurrency
+	// number of requests each client needs to send.
+	m := *total / n
+	log.Infof("concurrency: %d\nrequests per client: %d\n\n", n, m)
+
+	name := "Hello.Say"
+
+	args := prepareArgs()
+	//args := &rpc25519.BenchmarkMessage{}
+
+	// parameter size
+	b := make([]byte, 1024)
+	//i, _ := args.MarshalTo(b)
+	b, err := args.MarshalMsg(b)
+	if err != nil {
+		panic("could not MarshalMsg")
+	}
+	log.Infof("message size: %d bytes\n\n", len(b))
+
+	// Wait for all tests to complete.
+	var wg sync.WaitGroup
+	wg.Add(n * m)
+
+	// Total number of requests.
+	var trans uint64
+	// Return the total number of responses that were normal.
+	var transOK uint64
+
+	// Time consumption for each goroutine.
+	d := make([][]int64, n, n)
+
+	cfg := rpc25519.NewConfig()
+	cfg.ClientDialToHostPort = *host
+	cfg.TCPonly_no_TLS = true
+	cfg.SkipVerifyKeys = true
+
+	// create a client connection pool
+	var clientIndex uint64
+	poolClients := make([]*rpc25519.Client, 0, *pool)
+	for i := 0; i < *pool; i++ {
+
+		cli, err := rpc25519.NewClient(fmt.Sprintf("cli_%v", i), cfg)
+		if err != nil {
+			log.Infof("bad client config: '%v'\n", err)
+			os.Exit(1)
+		}
+		err = cli.Start()
+		if err != nil {
+			log.Infof("client could not connect: '%v'\n", err)
+			os.Exit(1)
+		}
+		defer cli.Close()
+		log.Infof("pool client %v connected from local addr='%v'\n", i, cli.LocalAddr())
+
+		/*		conn, err := net.Dial("tcp", *host)
+				if err != nil {
+					log.Fatal("dialing:", err)
+				}
+				c := codec.NewClientCodec(conn)
+				client := rpc.NewClientWithCodec(c)
+		*/
+
+		// warmup
+		var reply rpc25519.BenchmarkMessage
+		for j := 0; j < 5; j++ {
+			cli.Call(name, args, &reply, nil)
+			if j == 0 && i == 0 {
+				//fmt.Printf("reply = '%#v'\n", reply)
+			}
+		}
+		poolClients = append(poolClients, cli)
+	}
+
+	// Fence: control the client to start testing at the same time.
+	var startWg sync.WaitGroup
+	startWg.Add(n + 1) // +1 is because there is a goroutine to record the start time.
+
+	// create a client goroutine and test it.
+	startTime := time.Now().UnixNano()
+	go func() {
+		startWg.Done()
+		startWg.Wait()
+		startTime = time.Now().UnixNano()
+	}()
+
+	for i := 0; i < n; i++ {
+		dt := make([]int64, 0, m)
+		d = append(d, dt)
+
+		go func(i int) {
+			var reply rpc25519.BenchmarkMessage
+			startWg.Done()
+			startWg.Wait()
+
+			for j := 0; j < m; j++ {
+				// Current limiting, here the current waiting time is not counted into the waiting time.
+				if rl != nil {
+					rl.Take()
+				}
+
+				t := time.Now().UnixNano()
+				ci := atomic.AddUint64(&clientIndex, 1)
+				ci = ci % uint64(*pool)
+				client := poolClients[int(ci)]
+				err := client.Call(name, args, &reply, nil)
+				t = time.Now().UnixNano() - t
+
+				d[i] = append(d[i], t)
+
+				if err == nil && reply.Field1 == "OK" {
+					atomic.AddUint64(&transOK, 1)
+				}
+				// if err != nil {
+				// 	log.Error(err)
+				// }
+
+				atomic.AddUint64(&trans, 1)
+				wg.Done()
+			}
+		}(i)
+
+	}
+
+	wg.Wait()
+
+	// statistics
+	stat.Stats(startTime, *total, d, trans, transOK)
+}
+
+func prepareArgs() *rpc25519.BenchmarkMessage {
+	b := true
+	var i int32 = 100000
+	var s = "许多往事在眼前一幕一幕，变的那麼模糊"
+
+	var args rpc25519.BenchmarkMessage
+
+	v := reflect.ValueOf(&args).Elem()
+	num := v.NumField()
+	for k := 0; k < num; k++ {
+		field := v.Field(k)
+		if field.Type().Kind() == reflect.Ptr {
+			switch v.Field(k).Type().Elem().Kind() {
+			case reflect.Int, reflect.Int32, reflect.Int64:
+				field.Set(reflect.ValueOf(&i))
+			case reflect.Bool:
+				field.Set(reflect.ValueOf(&b))
+			case reflect.String:
+				field.Set(reflect.ValueOf(&s))
+			}
+		} else {
+			switch field.Kind() {
+			case reflect.Int, reflect.Int32, reflect.Int64:
+				field.SetInt(100000)
+			case reflect.Bool:
+				field.SetBool(true)
+			case reflect.String:
+				field.SetString(s)
+			}
+		}
+
+	}
+	return &args
+}
---
+++ rpc25519/server/srv25519.go
@@ -0,0 +1,123 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	//"io"
+	"log"
+	//"net"
+	"net/http"
+	_ "net/http/pprof"
+	//"net/rpc"
+	"runtime"
+	"time"
+
+	//codec "github.com/mars9/codec"
+	"github.com/glycerine/rpc25519"
+	//"github.com/rpcxio/rpcx-benchmark/proto"
+)
+
+//go:generate greenpack
+
+type Hello struct {
+	Placeholder int // greenpack will ignore if no public fields
+}
+
+func (t *Hello) Say(args *rpc25519.BenchmarkMessage, reply *rpc25519.BenchmarkMessage) error {
+	args.Field1 = "OK"
+	args.Field2 = 100
+	*reply = *args
+	if *delay > 0 {
+		time.Sleep(*delay)
+	} else {
+		runtime.Gosched()
+	}
+	//fmt.Printf("Hello.Say called!\n")
+	return nil
+}
+
+var (
+	host       = flag.String("s", "127.0.0.1:8972", "listened ip and port")
+	cpuprofile = flag.String("cpuprofile", "", "write cpu profile to file")
+	delay      = flag.Duration("delay", 0, "delay to mock business processing")
+	debugAddr  = flag.String("d", "127.0.0.1:9981", "server ip and port")
+)
+
+/*
+func mainOrigNetRpc() {
+	flag.Parse()
+
+	go func() {
+		log.Println(http.ListenAndServe(*debugAddr, nil))
+	}()
+
+	server := rpc.NewServer()
+	server.Register(new(Hello))
+
+	ln, err := net.Listen("tcp", *host)
+	if err != nil {
+		panic(err)
+	}
+
+	for {
+		conn, err := ln.Accept()
+		if err != nil {
+			log.Print("rpc.Serve: accept:", err.Error())
+			return
+		}
+		go serveConn(server, conn)
+	}
+}
+
+func serveConn(server *rpc.Server, conn io.ReadWriteCloser) {
+	srv := codec.NewServerCodec(conn)
+	server.ServeCodec(srv)
+}
+*/
+
+func main() {
+	flag.Parse()
+
+	go func() {
+		log.Println(http.ListenAndServe(*debugAddr, nil))
+	}()
+
+	cfg := rpc25519.NewConfig()
+	cfg.ServerAddr = *host
+	cfg.TCPonly_no_TLS = true
+	cfg.UseQUIC = false
+	cfg.SkipVerifyKeys = true
+
+	srv := rpc25519.NewServer("srv", cfg)
+	defer srv.Close()
+
+	//srv.Register2Func(customEcho)
+	srv.Register(new(Hello))
+
+	serverAddr, err := srv.Start()
+	if err != nil {
+		panic(fmt.Sprintf("could not start rpc25519.Server with config = '%#v'; err='%v'", cfg, err))
+	}
+	log.Printf("server listening on '%v'\n", serverAddr)
+	select {}
+}
+
+/*
+// echo implements rpc25519.TwoWayFunc
+func customEcho(req, reply *rpc25519.Message) error {
+
+	args.Field1 = "OK"
+	args.Field2 = 100
+	*reply = *args
+	if *delay > 0 {
+		time.Sleep(*delay)
+	} else {
+		runtime.Gosched()
+	}
+	return nil
+
+	//vv("callback to echo: with msg='%#v'", in)
+	//reply.JobSerz = append(req.JobSerz, []byte(fmt.Sprintf("\n with time customEcho sees this: '%v'", time.Now()))...)
+	//return nil
+}
+*/
